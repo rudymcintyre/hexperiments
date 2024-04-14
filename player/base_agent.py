5a735557516894a735c15b1abd9499b900be2be5
Rudy McIntyre <rudym335@gmail.com>
2024-04-14 16:57:45 +0100

    base_agent typehints and docstrings

--- player/base_agent.py
+++ player/base_agent.py
@@ -1,43 +1,70 @@
-from typing import Tuple
+from typing import Tuple, TypeVar
 
 
-class BaseAgent():
+BoardLike = list[list[TypeVar("T")]]
+Move = Tuple[int, int]
+
+
+class BaseAgent:
     """
-    Base class for agent including common functionality that 
+    Base class for agent including common functionality that
     may be used by all agents such as win condition checking
     """
-    board_state = None
-    colour = None
-    board_size = 6
+
+    board_state: BoardLike[str] = None
+    colour: str = None
+    board_size: int = 6
 
     def __init__(self, colour: str, board_size: int):
-        self.board_state = [['Empty' for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.board_state = [
+            ["Empty" for _ in range(self.board_size)] for _ in range(self.board_size)
+        ]
         self.colour = colour
         self.board_size = board_size
 
-    def win_condition(self, current_board, player) -> bool:
+    def win_condition(self, current_board: BoardLike, player: str) -> bool:
+        """Check if the player has won by DFS on 'wall' spaces of the colour
+        being checked
+
+        Args:
+            current_board (BoardLike): the current state of the board
+            player (str): the player to check if they have won
+
+        Returns:
+            bool: True if the player has a winning path
         """
-        Check if the player has won
-        """
-        visited = [[False for _ in range(self.board_size)] for _ in range(self.board_size)]
+        visited = [
+            [False for _ in range(self.board_size)] for _ in range(self.board_size)
+        ]
 
         for i in range(self.board_size):
-            if player == 'Red':
-                row = i
-                col = 0
-            else:
-                row = 0
-                col = i
+            row, col = (i, 0) if player == "Red" else (0, i)
 
             if current_board[row][col] == player and not visited[row][col]:
                 if self.dfs(row, col, current_board, player, visited):
                     return True
 
         return False
-    
-    def dfs(self, row: int, col: int, current_board, colour, visited: list[list[bool]]) -> bool:
-        """
-        Depth first search to find if the player has won
+
+    def dfs(
+        self,
+        row: int,
+        col: int,
+        current_board: BoardLike[int],
+        colour: str,
+        visited: BoardLike[bool],
+    ) -> bool:
+        """Depth first along nodes of the colour passed starting from row, col
+
+        Args:
+            row (int): the row to start the DFS from
+            col (int): the column to start the DFS from
+            current_board (BoardLike): the state to check
+            colour (str): the colour to check for win condition
+            visited (BoardLike): board of bools to keep track of visited nodes
+
+        Returns:
+            bool: True if the player has a connection, False otherwise
         """
         visited[row][col] = True
         stack = [(row, col)]
@@ -45,11 +72,16 @@ class BaseAgent():
         while stack:
             (r, c) = stack.pop()
 
-            for (nr, nc) in self.get_neighbours(r, c):
-                if (current_board[nr][nc] != colour):
+            for nr, nc in self.get_neighbours(r, c):
+                if current_board[nr][nc] != colour:
                     continue
 
-                if (colour == 'Blue' and nr == self.board_size - 1) or (colour == 'Red' and nc == self.board_size - 1):
+                if (
+                    colour == "Blue"
+                    and nr == self.board_size - 1
+                    or colour == "Red"
+                    and nc == self.board_size - 1
+                ):
                     return True
 
                 if not visited[nr][nc]:
@@ -58,33 +90,38 @@ class BaseAgent():
 
         return False
 
-    def get_neighbours(self, row: int, col: int) -> list[Tuple[int, int]]:
+    def get_neighbours(self, row: int, col: int) -> list[Move]:
+        """Get the neighbours of a given cell
+
+        Args:
+            row (int): the row of the cell
+            col (int): the column of the cell
+        """
+
         dirs = [(-1, 0), (-1, 1), (0, 1), (1, 0), (1, -1), (0, -1)]
         neighbours = []
 
-        for (dx, dy) in dirs:
-            if 0 <= row + dx < self.board_size and 0 <= col + dy < self.board_size:
-                neighbours.append((row + dx, col + dy))
-        
+        for dx, dy in dirs:
+            new_row, new_col = row + dx, col + dy
+
+            if 0 <= new_row < self.board_size and 0 <= new_col < self.board_size:
+                neighbours.append((new_row, new_col))
+
         return neighbours
 
+    def get_opponent(self, colour: str) -> str:
+        """Get the opponent colour
 
-    def get_opponent(self, colour) -> str:
+        Args:
+            colour (str): the colour of the player
         """
-        Get the opponent colour
-        """
-        return 'Red' if colour == 'Blue' else 'Blue'
+        return "Red" if colour == "Blue" else "Blue"
 
+    def update_game_state(self, game_state: BoardLike[str]):
+        """Setter for game state"""
 
-    def update_game_state(self, game_state: list[list[str]]):
-        """
-        Setter for game state
-        """
         self.board_state = game_state
 
-    def get_move(self, *args, **kwargs) -> Tuple[int, int]:
-        """
-        Abstract function to be overridden by each type of child class
-        """
+    def get_move(self) -> Move:
+        """Abstract function to be overridden by each type of child class"""
         raise NotImplementedError()
-    
